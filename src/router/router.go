0a08afe5ce94ee5406ba76f8fbaed5c62c41bec7
LucasPerezAlves <105084986+LucasPerezAlves@users.noreply.github.com>
2023-04-07 22:36:27 -0300

    Feature/inicio project (#2)
    * inicialização do projeto // Configurado as rotas e feito alguns passos para o usuário
    
    * updated
    
    * adicionado a pasta modelos com um struct de usuario

--- src/router/router.go
+++ src/router/router.go
@@ -1,5 +1,10 @@
 package router
 
+// Gerar vai retornar um Router com as rotas configuradas
+func Gerar() *mux.Router {
+	r := mux.NewRouter()
+	return rotas.Configurar(r)
+
 import "github.com/gorilla/mux"
 
 //Gerar vai retornar um Router com as rotas configuradas
